e02151df9a249959083e7e35a5fb360c4c646817
Alonso de Oliveira <alonsoboj@gmail.com>
2023-08-20 02:35:38 -0300

    (feat): adds AutoMapper.

---
+++ ACME.Store.Application/Configuration/AutoMapper/CustomerProfile.cs
@@ -0,0 +1,16 @@
+﻿using ACME.Store.Application.Extensions;
+using ACME.Store.Domain.Dtos.Requests;
+using ACME.Store.Domain.Entities;
+using AutoMapper;
+
+namespace ACME.Store.Application.Configuration.AutoMapper;
+
+public class CustomerProfile : Profile
+{
+    public CustomerProfile()
+    {
+        CreateMap<RegisterCustomerRequestDto, Customer>()
+        .IgnoreBaseEntityProperties()
+        .ForMember(destination => destination.Addresses, opt => opt.Ignore());
+    }
+}
---
+++ ACME.Store.Application/Extensions/AutoMapper.cs
@@ -0,0 +1,29 @@
+﻿using ACME.Store.Domain.Entities;
+using AutoMapper;
+using System.Linq;
+
+namespace ACME.Store.Application.Extensions;
+
+public static class AutoMapper
+{
+    public static IMappingExpression<TSource, TDestination> IgnoreBaseEntityProperties<TSource, TDestination>(
+        this IMappingExpression<TSource, TDestination> mappingExpression)
+        where TDestination : new()
+    {
+        var destinationProperties = typeof(TDestination).GetProperties();
+
+        var baseEntityProperties = typeof(BaseEntity).GetProperties();
+
+        foreach (var baseEntityProperty in baseEntityProperties)
+        {
+            var matchingProperty = destinationProperties.FirstOrDefault(p => p.Name == baseEntityProperty.Name);
+
+            if (matchingProperty != null)
+            {
+                mappingExpression.ForMember(matchingProperty.Name, opt => opt.Ignore());
+            }
+        }
+
+        return mappingExpression;
+    }
+}
